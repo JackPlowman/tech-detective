fd5b8406fb0018ae4e39331b2da1fa418ed218d7
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-17 21:18:56 +0100

    Add Ruff Settings

--- detector/application/__main__.py
+++ detector/application/__main__.py
@@ -1,7 +1,8 @@
 from application.utils.custom_logging import set_up_custom_logging
 
 
-def main():
+def main() -> None:
+    """Main function for the application."""
     set_up_custom_logging()
 
 
