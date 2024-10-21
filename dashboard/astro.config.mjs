45695c5f0264442d2e320df183294f99eb0b09c9
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-21 13:07:58 +0100

    build: Add robots.txt integration to Astro config (#84)
    This change integrates the `astro-robots-txt` package into the project
    to generate a robots.txt file for the website. The following
    modifications were made:
    
    1. Added `astro-robots-txt` as a dependency in `package.json` and
    `package-lock.json`.
    2. Updated `astro.config.mjs` to import and use the `robotsTxt`
    integration.
    
    These changes will automatically generate a robots.txt file during the
    build process, improving the site's SEO and crawler management
    capabilities.
    
    fixes #82

--- dashboard/astro.config.mjs
+++ dashboard/astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jackplowman.github.io",
   base: "tech-detective",
+  integrations: [robotsTxt()],
 });
