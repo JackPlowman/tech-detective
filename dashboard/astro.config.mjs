d78dace6273e63e0287585599482ce460fd29e85
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-21 13:28:12 +0100

    build: Add web manifest integration to Astro config (#89)
    # Pull Request
    
    ## Description
    
    This PR adds a web app manifest to the Tech Detective project. The changes include:
    
    1. Adding the `astro-webmanifest` integration to the Astro configuration file.
    2. Configuring the web app manifest with details such as name, icons, description, and theme colours.
    3. Installing the `astro-webmanifest` package as a project dependency.
    
    These changes will enable the Tech Detective to be installed as a Progressive Web App (PWA) on supported devices, providing a more native app-like experience for users.
    
    fixes #88

--- dashboard/astro.config.mjs
+++ dashboard/astro.config.mjs
@@ -1,10 +1,24 @@
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import webmanifest from "astro-webmanifest";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://jackplowman.github.io",
   base: "tech-detective",
-  integrations: [robotsTxt(), sitemap()],
+  integrations: [
+    robotsTxt(),
+    sitemap(),
+    webmanifest({
+      name: "Tech Detective",
+      icon: "public/favicon.svg",
+      short_name: "Tech Detective",
+      description: "What Technologies are used in my projects?",
+      start_url: "/tech-detective",
+      theme_color: "#3367D6",
+      background_color: "#3367D6",
+      display: "standalone",
+    }),
+  ],
 });
