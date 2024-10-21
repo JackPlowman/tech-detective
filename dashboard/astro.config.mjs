e5ef2b515d654346ca04fd2fc5ec1b9945e2f9f1
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-21 12:42:32 +0100

    Add sitemap

--- dashboard/astro.config.mjs
+++ dashboard/astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from "astro/config";
 import robotsTxt from "astro-robots-txt";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://jackplowman.github.io",
   base: "tech-detective",
-  integrations: [robotsTxt()],
+  integrations: [robotsTxt(), sitemap()],
 });
