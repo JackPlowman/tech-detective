585dee853767a0438b7ba69929b7d22a5388fbde
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-21 12:53:51 +0100

    Add TypeScript import formatting

--- dashboard/astro.config.mjs
+++ dashboard/astro.config.mjs
@@ -1,8 +1,6 @@
-// @ts-check
-import { defineConfig } from "astro/config";
-import robotsTxt from "astro-robots-txt";
-
 import sitemap from "@astrojs/sitemap";
+import robotsTxt from "astro-robots-txt";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
