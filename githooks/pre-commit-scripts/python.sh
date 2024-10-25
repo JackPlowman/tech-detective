ddbe8f9562fb8f5020663cc5d391998ce86b7cf2
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-25 15:26:43 +0100

    Add GitHooks for Tests

--- githooks/pre-commit-scripts/python.sh
+++ githooks/pre-commit-scripts/python.sh
@@ -6,3 +6,9 @@ if git diff --cached --name-only | grep -q "^detector/"; then
   just detector::install
   just detector::ruff-fix
 fi
+
+# Check if there are any changes in the watched folder
+if git diff --cached --name-only | grep -q "^tests/"; then
+  just tests::install
+  just tests::ruff-fix
+fi
