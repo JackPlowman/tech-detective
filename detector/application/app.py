aca6df5cb5dbcb82c7973fd82bb1581538728876
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-10-29 20:47:55 +0000

    Temp Streamline Detector

--- detector/application/app.py
+++ detector/application/app.py
@@ -8,5 +8,13 @@ logger: stdlib.BoundLogger = get_logger()
 def generate_tech_report() -> str:
     """Generate a report on the technologies used in the repository."""
     repositories = retrieve_repositories()
-    for repository in repositories:
-        scrape_technologies(repository)
+
+    # Temporary code to test the scraper
+    tech_detective = next(
+        repository for repository in repositories if repository.full_name == "JackPlowman/tech-detective"
+    )
+    scrape_technologies(tech_detective)
+
+    # Reimplement this
+    # for repository in repositories:
+    #     scrape_technologies(repository) # noqa: ERA001
--- detector/application/tests/test_app.py
+++ detector/application/tests/test_app.py
@@ -9,9 +9,10 @@ FILE_PATH = "detector.application.app"
 @patch(f"{FILE_PATH}.retrieve_repositories")
 def test_generate_tech_report(mock_retrieve_repositories: MagicMock, mock_scrape_technologies: MagicMock) -> None:
     # Arrange
-    mock_retrieve_repositories.return_value = ["test"]
+    tech_detective = MagicMock(full_name="JackPlowman/tech-detective")
+    mock_retrieve_repositories.return_value = [tech_detective]
     # Act
     generate_tech_report()
     # Assert
     mock_retrieve_repositories.assert_called_once_with()
-    mock_scrape_technologies.assert_called_once_with("test")
+    mock_scrape_technologies.assert_called_once_with(tech_detective)
