e5cde43aa8066af3b803adf04befcf877e4c92cd
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2024-11-02 09:08:45 +0000

    Update

--- detector/application/tests/test_app.py
+++ detector/application/tests/test_app.py
@@ -5,11 +5,15 @@ from detector.application.app import generate_tech_report
 FILE_PATH = "detector.application.app"
 
 
+@patch(f"{FILE_PATH}.summarise_tech_report")
 @patch(f"{FILE_PATH}.generate_output_file")
 @patch(f"{FILE_PATH}.scrape_technologies")
 @patch(f"{FILE_PATH}.retrieve_repositories")
 def test_generate_tech_report(
-    mock_retrieve_repositories: MagicMock, mock_scrape_technologies: MagicMock, mock_generate_output_file: MagicMock
+    mock_retrieve_repositories: MagicMock,
+    mock_scrape_technologies: MagicMock,
+    mock_generate_output_file: MagicMock,
+    mock_summarise_tech_report: MagicMock,
 ) -> None:
     # Arrange
     tech_detective = MagicMock(full_name="JackPlowman/tech-detective")
@@ -19,4 +23,4 @@ def test_generate_tech_report(
     # Assert
     mock_retrieve_repositories.assert_called_once_with()
     mock_scrape_technologies.assert_called_once_with(tech_detective)
-    mock_generate_output_file.assert_called_once_with([mock_scrape_technologies.return_value])
+    mock_generate_output_file.assert_called_once_with(mock_summarise_tech_report.return_value)
